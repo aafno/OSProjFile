b58952ee77460e13e404c964e5439aa294db1ecb
Prasanna.Bhattarai <aafnoo@gmail.com>
2012-03-27 16:52:57 -0400

    first set of files

---
+++ OSProject/type_def.h
@@ -0,0 +1,9 @@
+#
+
+typedef unsigned long __u32;
+typedef unsigned long u32;
+typedef unsigned int __u16;
+typedef unsigned short __u8;
+typedef long __s32;
+typedef int __s16;
+typedef short __s8;
